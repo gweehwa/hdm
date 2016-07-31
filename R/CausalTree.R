8ed982768876e6fe7c80b7b7598a0865bbe6cc33
gweehwa <gweehwa@gmail.com>
2016-07-31 14:49:54 -0400

    Update and rename causalTree.R to CausalTree.R

--- R/CausalTree.R
+++ R/CausalTree.R
@@ -3,7 +3,7 @@
 #
 # 
 
-causalTree <- function(formula, data, weights, treatment, subset, 
+CausalTree <- function(formula, data, weights, treatment, subset, 
 					   na.action = na.causalTree, 
 					   split.Rule, split.Honest, HonestSampleSize, split.Bucket, bucketNum = 5,
 					   bucketMax = 100, cv.option, cv.Honest, minsize = 2L, 
