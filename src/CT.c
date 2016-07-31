49e1ec38012b48ec4a8d100781221c7203d94dc3
gweehwa <gweehwa@gmail.com>
2016-07-31 10:55:43 -0400

    Update CT.c

--- src/CT.c
+++ src/CT.c
@@ -14,7 +14,7 @@ static double *wtsqrsums, *trsqrsums;
 int
 CTinit(int n, double *y[], int maxcat, char **error,
         int *size, int who, double *wt, double *treatment, 
-        int bucketnum, int bucketMax, double *train_to_est_ratio)
+        int bucketnum, int bucketMax, double *train_to_est_ratio, double *propensity) /*add propensity*/
 {
     if (who == 1 && maxcat > 0) {
         graycode_init0(maxcat);
