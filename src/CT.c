f927fe444729bb85191d6cfb8fe9e0e03547b0c3
gweehwa <gweehwa@gmail.com>
2016-07-31 16:20:16 -0400

    Update CT.c

--- src/CT.c
+++ src/CT.c
@@ -316,7 +316,7 @@ void CT(int n, double *y[], double *x, int nclass, int edge, double *improve, do
                 temp = left_effect + right_effect - node_effect;
             
                 
-                if (temp > best*1000) {
+                if (temp > 100000) {
                     best = temp;
                     
                     if (left_temp > right_temp)
